007b88a905ea4cb5a2d4a8a1efd4381b02427391
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-05 19:07:29 -0500

    select bin and form

---
+++ app.js
@@ -0,0 +1,21 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+console.log(data)
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+  var button = d3.select("#filter-btn")
+  var input = d3.select("form")
+
+  button.on("click",runFilter);
+  input.on("submit",runFilter);
