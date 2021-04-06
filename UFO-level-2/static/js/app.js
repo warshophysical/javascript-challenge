15f9f4755b1018993e0e1ec1d514ac40602e8511
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-05 19:30:38 -0500

    Add files via upload

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,4 +17,29 @@ data.forEach((ufoSighting) => {
   var button = d3.select("#filter-btn")
   var input = d3.select("form")
 
+  button.on("click",runFilter);
+  input.on("submit",runFilter);
+
+  function runFilter(){
+    d3.event.preventDefault();
+    inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    tbody.html("")
+
+    filteredData.forEach((ufoSighting)=> {
+      var row = tbody.append("tr");
+      Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+
+}
+
   
